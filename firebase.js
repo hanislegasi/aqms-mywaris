c3c7b578bd7191b8a808b017932d4e2877b4a7bc
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-05-24 22:29:20 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+
+ apiKey:"",
+
+ authDomain:"",
+
+ projectId:"",
+
+ storageBucket:"",
+
+ messagingSenderId:"",
+
+ appId:""
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
