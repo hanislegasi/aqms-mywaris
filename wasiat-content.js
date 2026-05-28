c4c442ccaeace3d2aad91697e353e4f8c7c75b18
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-05-28 22:42:02 +0800

    Create wasiat-content.js

---
+++ wasiat-content.js
@@ -0,0 +1,90 @@
+const wasiatContent = `
+
+<div class="mt-20 px-16">
+
+<h2
+style="
+font-family:'Cinzel', serif;
+font-size:38px;
+font-weight:700;
+margin-bottom:40px;
+color:#153638;
+text-align:center;
+">
+DOKUMEN WASIAT
+</h2>
+
+<div
+style="
+font-family:'Cinzel', serif;
+font-size:18px;
+line-height:42px;
+text-align:justify;
+color:#222;
+">
+
+<p style="text-align:center; margin-bottom:40px;">
+Dengan Nama Allah yang Maha Pemurah Lagi Mengasihani.
+</p>
+
+<p>
+Segala puji bagi Allah S.W.T yang kekal selamanya. Tiada tuhan melainkan Allah S.W.T, Nabi Muhammad S.A.W itu adalah pesuruh Allah.
+</p>
+
+<p>
+Ini adalah Dokumen Pewarisan Lengkap bagi diri saya.
+</p>
+
+<h3 style="margin-top:50px; margin-bottom:20px;">
+1. Pengisytiharan Keadaan Diri
+</h3>
+
+<p>
+Dengan disaksikan oleh saksi-saksi yang bertandatangan dibawah, saya mengakui bahawa saya adalah seorang Islam yang cukup umur, berada di dalam keadaan sihat tubuh badan dan waras akal fikiran.
+</p>
+
+<p>
+Saya memahami sepenuhnya dokumen ini serta kesannya menurut hukum syarak dan undang-undang Malaysia.
+</p>
+
+<h3 style="margin-top:50px; margin-bottom:20px;">
+2. Pelantikan Wasi
+</h3>
+
+<p>
+Saya dengan ini melantik wasi dan pemegang amanah bagi harta pusaka saya.
+</p>
+
+<h3 style="margin-top:50px; margin-bottom:20px;">
+3. Tanggungjawab Wasi
+</h3>
+
+<p>
+Wasi hendaklah menguruskan segala urusan pentadbiran pusaka saya dengan penuh amanah dan tanggungjawab.
+</p>
+
+<p>
+Wasi juga hendaklah menyelesaikan segala hutang serta tanggungan saya sebelum pembahagian dibuat kepada waris-waris.
+</p>
+
+<h3 style="margin-top:50px; margin-bottom:20px;">
+4. Kaedah Pembahagian Harta
+</h3>
+
+<p>
+Harta pusaka saya hendaklah diagihkan mengikut hukum faraid dan tertakluk kepada hukum syarak.
+</p>
+
+<h3 style="margin-top:50px; margin-bottom:20px;">
+5. Pesanan Kepada Waris
+</h3>
+
+<p>
+Saya berpesan kepada waris-waris saya supaya menjaga hubungan silaturrahim serta menggunakan harta pusaka dengan penuh amanah.
+</p>
+
+</div>
+
+</div>
+
+`;
