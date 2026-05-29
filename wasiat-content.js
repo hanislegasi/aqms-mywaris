d1dbe1f2df1315a53b00dafa1d2a2c677a5a5063
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-05-29 10:32:14 +0800

    Update wasiat-content.js

--- wasiat-content.js
+++ wasiat-content.js
@@ -55,7 +55,15 @@ Saya memahami sepenuhnya dokumen ini serta kesannya menurut hukum syarak dan und
 </h3>
 
 <p>
-Saya dengan ini melantik wasi dan pemegang amanah bagi harta pusaka saya.
+Maka saya dengan ini:
+</p>
+<p>
+Melantik {{HUBUNGAN_WASI1}} saya iaitu {{NAMA_WASI1}} No. K/P {{NO_IC_WASI1}} sebagai WASI PERTAMA dan pemegang amanah bagi harta pusaka saya.
+</p>
+<p>
+Sekiranya, dalam apa jua keadaan di mana Wasi Pertama di atas telah meninggal dunia atau hilang upaya daripada menjalankan tanggungjawab sebagai Wasi, 
+ATAU tidak dapat memulakan (initiate) proses pentadbiran bagi harta pusaka saya dalam masa 60 hari selepas kematian saya, 
+maka saya dengan ini melantik {{HUBUNGAN_WASI2}} saya iaitu {{NAMA_WASI2}} No. K/P {{NO_IC_WASI2}} sebagai Pentadbir gantian.
 </p>
 
 <h3 style="margin-top:50px; margin-bottom:20px;">
