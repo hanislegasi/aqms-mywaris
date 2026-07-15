dfd4639be738e9083333c1271ec4b0ce069df04e
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-07-15 12:41:33 +0800

    Add footer to PDF content in hibah-content.js
    Added PDF footer section to hibah-content.js

--- hibah-content.js
+++ hibah-content.js
@@ -50,6 +50,15 @@ Walaupun begitu, sekiranya wujud keperluan di mana suatu Perintah Pengesahan Hib
 
 <p>Bahawasanya SURAT IKATAN HIBAH DAN AMANAH ini adalah pengisytiharan terakhir dan mana-mana pengisytiharan hibah yang telah dinyatakan terdahulu ke atas harta hibah ini adalah terbatal.</p>
 
+<div class="pdf-footer">
+
+<span class="footer-docref"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
 
 <br>
 
