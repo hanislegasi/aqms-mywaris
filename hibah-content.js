4a6a1b1597514d5d2e1d96b3f7d06cbb60d43cb5
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-07-16 12:46:44 +0800

    Add PEMEGANG AMANAH section to hibah-content.js

--- hibah-content.js
+++ hibah-content.js
@@ -196,6 +196,12 @@ Saya juga faham dan mengesahkan, walaupun dinyatakan di dalam Dokumen Pengisytih
 Bahawa Pemberi Hibah Amanah secara sedar, waras, dengan sukarelanya dan bukan dalam keadaan sakit, atas dasar kasih sayang dan tanpa mendapat apa-apa balasan dengan ini membuat hibah ke atas Harta Amanah tersebut kepada Penerima Hibah dengan kadar seperti di dalam Jadual 1.</p>
 
 <h3 style="margin-top:40px; margin-bottom:15px; font-weight:bold;">
+<strong>2.	PEMEGANG AMANAH</strong>
+</h3>
+
+{{PEMEGANG_AMANAH}}
+
+<h3 style="margin-top:40px; margin-bottom:15px; font-weight:bold;">
 <strong>2. AQAD</strong>
 </h3>
 
@@ -420,6 +426,8 @@ SENARAI HARTA TAK ALIH YANG DIHIBAHKAN
 </div>
 
 </div>
+
+
 
 `;
 
