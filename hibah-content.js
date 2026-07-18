f708a812576daa902725196ffb411632538df89a
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-07-18 12:42:44 +0800

    Enhance hibah-content.js with footer and page layout
    Added footer and page structure to hibah-content.js

--- hibah-content.js
+++ hibah-content.js
@@ -546,6 +546,13 @@ Penerima Hibah mengakui bahawa Pemberi Hibah menandatangani Perjanjian Hibah ini
 
 <p>Perjanjian Hibah ini adalah dikuasai dan ditafsirkan menurut undang-undang di Malaysia dan tidak berlawanan dengan Hukum Syarak dan kedua-dua pihak adalah dengan ini patuh kepada bidang kuasa Mahkamah-Mahkamah di Malaysia.</p>
 
+<div class="pdf-footer">
+<span class="footer-docref"></span>
+<span class="page-number"></span>
+</div>
+</div>
+
+<div class="a4-page p-[18mm]">
 
 <h3
 style="
