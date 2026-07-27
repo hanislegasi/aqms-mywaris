0e7e93745cf7e3abaee3b6221721225b05100488
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-07-27 16:06:20 +0800

    Refactor generateLampiran function to use window scope

--- lampiran-content.js
+++ lampiran-content.js
@@ -1,39 +1,16 @@
-async function generateLampiran(lampiranSnap) {
+window.generateLampiran = async function(lampiranSnap){
 
     let lampiranHTML = "";
     let hurufLampiran = "I".charCodeAt(0);
 
-    for (const doc of lampiranSnap.docs) {
+    for(const doc of lampiranSnap.docs){
 
         const file = doc.data();
-        const base64Image = await imageToBase64(file.fileURL);
 
-        lampiranHTML += `
-        <div class="a4-page p-[18mm]">
+        const base64Image =
+        await imageToBase64(file.fileURL);
 
-            <h2 style="font-size:22px;font-weight:bold;margin-bottom:15px;">
-                Lampiran ${String.fromCharCode(hurufLampiran)}
-            </h2>
-
-            <p style="margin-bottom:20px;font-size:15px;">
-                ${file.fileName}
-            </p>
-
-            <img
-                src="${base64Image}"
-                style="
-                    display:block;
-                    max-width:170mm;
-                    max-height:220mm;
-                    margin:20px auto;
-                    border:1px solid #ccc;
-                    object-fit:contain;
-                ">
-
-            <div class="pdf-footer"></div>
-
-        </div>
-        `;
+        lampiranHTML += `...`;
 
         hurufLampiran++;
     }
