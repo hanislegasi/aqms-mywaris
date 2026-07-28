fca8e7d7cda8fe81d7dd9e8214a8d7d7cd86a615
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-07-28 08:10:04 +0800

    Refactor lampiran content structure and add sections
    Updated lampiran content structure to include multiple sections with headers and placeholders for various lampiran types.

--- lampiran-content.js
+++ lampiran-content.js
@@ -1,20 +1,220 @@
-window.generateLampiran = async function(lampiranSnap){
+const lampiranContent = `
 
-    let lampiranHTML = "";
-    let hurufLampiran = "I".charCodeAt(0);
+<div class="a4-page p-[18mm]">
 
-    for(const doc of lampiranSnap.docs){
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN A : SENARAI WARIS
+</h2>
 
-        const file = doc.data();
+<div id="lampiranWaris">
+{{LAMPIRAN_WARIS}}
+</div>
 
-        const base64Image =
-        await imageToBase64(file.fileURL);
+<div class="pdf-footer">
 
-        lampiranHTML += `...`;
+<span class="docRef"></span>
 
-        hurufLampiran++;
-    }
+<span class="page-number"></span>
 
-    return lampiranHTML;
+</div>
 
-}
+</div>
+
+<div class="a4-page p-[18mm]">
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN B : PEMEGANG AMANAH
+</h2>
+
+<div>
+{{LAMPIRAN_PEMEGANG_AMANAH}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+<div class="a4-page p-[18mm]">
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN C : HARTA ALIH
+</h2>
+
+<div>
+{{LAMPIRAN_HARTA_ALIH}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+<div class="a4-page p-[18mm]">
+
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN D : HARTA TAK ALIH
+</h2>
+
+<div>
+{{LAMPIRAN_HARTA_TAK_ALIH}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+<div class="a4-page p-[18mm]">
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN E : HARTA BERPENAMA
+</h2>
+
+<div>
+{{LAMPIRAN_BERPENAMA}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+<div class="a4-page p-[18mm]">
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN F : TAKAFUL
+</h2>
+
+<div>
+{{LAMPIRAN_TAKAFUL}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+<div class="a4-page p-[18mm]">
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN G : PEMIUTANG
+</h2>
+
+<div>
+{{LAMPIRAN_PEMIUTANG}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+<div class="a4-page p-[18mm]">
+
+<h2
+style="
+font-family:'Times New Roman', serif;
+font-size:28px;
+margin-top:30px;
+margin-bottom:20px;
+color:#153638;
+">
+LAMPIRAN H : PENGHUTANG
+</h2>
+
+<div>
+{{LAMPIRAN_PENGHUTANG}}
+</div>
+
+<div class="pdf-footer">
+
+<span class="docRef"></span>
+
+<span class="page-number"></span>
+
+</div>
+
+</div>
+
+`;
