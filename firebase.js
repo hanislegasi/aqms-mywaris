71ddd29a171d9ff926ab6b8779973c05926fbf11
hanislegasi <hasnawi.hanslegasi@gmail.com>
2026-08-13 15:09:18 +0800

    Update Firebase configuration with new credentials

--- firebase.js
+++ firebase.js
@@ -6,17 +6,12 @@ import { getAuth } from
 
 const firebaseConfig = {
 
- apiKey:"",
-
- authDomain:"",
-
- projectId:"",
-
- storageBucket:"",
-
- messagingSenderId:"",
-
- appId:""
+apiKey: "AIzaSyCR3hpOyMowg4yLMxBw3Iu3QkepGvSE0MU",
+authDomain: "alqawthar-mywaris.firebaseapp.com",
+projectId: "alqawthar-mywaris",
+storageBucket: "alqawthar-mywaris.firebasestorage.app",
+messagingSenderId: "3498810778",
+appId: "1:3498810778:web:ee36e0fe42f76698046e23"
 
 };
 
